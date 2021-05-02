d1a86508e10ffba5872c9c662a03e0ba9e5ce063
Hanna-Yoo <sung17cse58@gmail.com>
2021-05-02 19:32:38 +0900

    코스상세정보 디자인 수정

--- app/src/main/java/com/example/ttubeog/BottomFragment2.java
+++ app/src/main/java/com/example/ttubeog/BottomFragment2.java
@@ -1,16 +1,46 @@
 package com.example.ttubeog;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.model.Document;
+
+import java.lang.reflect.Array;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 public class BottomFragment2 extends Fragment {
+
+    String TAG = "BottomFragment2_log";
+    String content;
+    GeoPoint location;
+    Array tag;
+    int time;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
@@ -21,5 +51,24 @@ public class BottomFragment2 extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("course")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        // 데이터를 가져오는 작업이 잘 동작했을 떄
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        }
+                        // 데이터를 가져오는 작업이 에러났을 때
+                        else {
+                            Log.w(TAG, "Error => ", task.getException());
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/ttubeog/information.java
+++ app/src/main/java/com/example/ttubeog/information.java
@@ -2,12 +2,74 @@ package com.example.ttubeog;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+
+import java.lang.reflect.Array;
 
 
-public class information extends Activity {
+public class information extends Activity implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private static NaverMap naverMap;
+
+    String TAG = "information_log";
+    String content;
+    GeoPoint location;
+    Array tag;
+    int time;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_information);
+
+        mapView = (MapView) findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("course")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        // 데이터를 가져오는 작업이 잘 동작했을 떄
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                //Log.d(TAG, document.getId() + " => " + document.getData());
+                                if (document.getId() == "test1") {
+                                    location = (GeoPoint) document.get("location");
+                                    tag = (Array) document.get("tag");
+                                    time = (int) document.get("time");
+                                    content = (String) document.get("content");
+                                }
+                            }
+                        }
+                        // 데이터를 가져오는 작업이 에러났을 때
+                        else {
+                            Log.w(TAG, "Error => ", task.getException());
+                        }
+                    }
+                });
+
+        Log.d(TAG, time + content);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
     }
 }
